4b805a03229d4e939ebaf0c7a1de3d0fb6e64864
sayitoDeng <deng_baokang@hotmail.com>
2020-08-28 19:58:58 +0800

    webpack构建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <ol>
+      <li
+        v-for="item in groceryList"
+        v-bind:todo="item"
+        v-bind:key="item.id"
+      >{{item.text}}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  components:{
+  },
+  data () {
+    return {
+      groceryList:[{ id: 0, text: '蔬菜' },
+        { id: 1, text: '奶酪' },
+        { id: 2, text: '随便其它什么人吃的东西' }]
+        
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
